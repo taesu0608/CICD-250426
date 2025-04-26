4fe08c5d7c3455594fd1222401577f0d4564edb4
taesu <taesu0608@gmail.com>
2025-04-26 16:31:18 +0900

    init commit

---
+++ src/main/java/org/example/cicd250426/Cicd250426Application.java
@@ -0,0 +1,13 @@
+package org.example.cicd250426;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cicd250426Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cicd250426Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/cicd250426/Cicd250426ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cicd250426;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cicd250426ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
