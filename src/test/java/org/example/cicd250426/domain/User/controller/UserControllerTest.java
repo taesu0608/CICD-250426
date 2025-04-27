b3009d44df6f28709d84a5bfcb39cede4837107c
taesu <taesu0608@gmail.com>
2025-04-27 15:30:18 +0900

    fix: test

--- src/test/java/org/example/cicd250426/domain/User/controller/UserControllerTest.java
+++ src/test/java/org/example/cicd250426/domain/User/controller/UserControllerTest.java
@@ -2,13 +2,16 @@ package org.example.cicd250426.domain.User.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(UserController.class)  // 🔥 UserController만 테스트
+@SpringBootTest
+@AutoConfigureMockMvc
 class UserControllerTest {
 
     @Autowired
