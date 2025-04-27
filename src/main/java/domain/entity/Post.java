1beab095ce7c0514683362f14d5965aae717dfd1
taesu <taesu0608@gmail.com>
2025-04-27 10:27:51 +0900

    lombok 의존성 추가

--- src/main/java/domain/entity/Post.java
+++ src/main/java/domain/entity/Post.java
@@ -1,4 +1,14 @@
 package domain.entity;
 
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Getter
 public class Post{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
